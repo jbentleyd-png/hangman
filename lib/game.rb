f0a5bd4a9b7b99d391d29e5da5baae68f026c27f
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-05-24 15:09:41 -0600

    Capped number of saved gamees possible.
    Only 3 allowed.

--- lib/game.rb
+++ lib/game.rb
@@ -18,6 +18,12 @@ class Game
     save_string = JSON.generate(self.to_hash)
     save_dir = File.join(__dir__, "..", "saved_games")
     save_number = Dir.glob(File.join(save_dir, "*json")).length # length of array of all .jsons in that directory
+    if save_number >= 3 
+      puts "Insufficient space. May not save more than 3 games.".red
+      return
+    else 
+      print "Game saved. ".green
+    end
     while File.exist?(File.join(save_dir, "save#{save_number}.json"))
       save_number += 1
     end
@@ -53,7 +59,6 @@ class Game
     case letter
     when "SAVE"
       self.save
-      print "Game saved. ".green
       puts "Exiting game.".red
       return 'exited'
     when "EXIT"
